6834b2b22f67fe78061b89617d3f799b1af6767c
Ernesto Medina <ernesto.m.medina.m@gmail.com>
2021-06-08 00:02:58 +0000

    :tada: Commit from class

--- src/main.py
+++ src/main.py
@@ -9,16 +9,23 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
+from flask_jwt_extended import create_access_token, JWTManager
+
+
+# Setup the Flask-JWT-Extended extension
+
 #from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["JWT_SECRET_KEY"] = "01cdeef14f0a17d28d723f35a2ba3670"
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
+jwt = JWTManager(app)
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -38,6 +45,31 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@app.route("/sign-up", methods=["POST"])
+def sign_up():
+    data = request.json
+    user = User.create(email=data.get('email'), password=data.get('password'))
+    if not isinstance(user, User):
+        return jsonify({"msg": "tuve problemas, lo siento"}), 500
+    return jsonify(user.serialize()), 201
+
+@app.route("/log-in", methods=["POST"])
+def log_in():
+    print(request.data)
+    print(request.json)
+    data = request.json
+    user = User.query.filter_by(email=data['email']).one_or_none()
+    if user is None: 
+        return jsonify({"msg": "no existe el usuario"}), 404
+    if not user.check_password(data.get('password')):
+        return jsonify({"msg": "bad credentials"}), 400
+    token = create_access_token(identity=user.id)
+    return jsonify({
+        "user": user.serialize(),
+        "token": token
+    }), 200
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,3 +1,5 @@
+import os
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -5,8 +7,41 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    salt = db.Column(db.String(40), nullable=False)
+    hashed_password = db.Column(db.String(240), nullable=False)
+    is_active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, **kwargs):
+        print(kwargs)
+        self.email = kwargs.get('email')
+        self.salt = os.urandom(16).hex()
+        self.set_password(kwargs.get('password'))
+
+    @classmethod
+    def create(cls, **kwargs):
+        user = cls(**kwargs)
+        db.session.add(user)
+        try: 
+            db.session.commit()
+        except Exception as error:
+            print(error.args)
+            db.session.rollback()
+            return False
+        return user
+
+    def set_password(self, password):
+        print(generate_password_hash(
+            f"{password}{self.salt}"
+        ))
+        self.hashed_password = generate_password_hash(
+            f"{password}{self.salt}"
+        )
+
+    def check_password(self, password):
+        return check_password_hash(
+            self.hashed_password,
+            f"{password}{self.salt}"
+        )
 
     def __repr__(self):
         return '<User %r>' % self.username
